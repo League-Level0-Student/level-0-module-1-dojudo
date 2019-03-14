8018dd0b361bc5722e13d4171bd9ab90bde6d508
ilabstaff <ilabstaff@172.16.116.67>
2019-03-13 17:53:45 -0700

    riddler

--- src/greeter.java
+++
@@ -1,8 +0,0 @@
-import javax.swing.JOptionPane;
-
-public class greeter {
-public static void main(String[] args) {
-	String name=JOptionPane.showInputDialog("Enter your name");
-	JOptionPane.showMessageDialog(null,"hello"+ name);
-}
-}
--- src/ifs_ints_and_loops/TheRiddler.java
+++ src/ifs_ints_and_loops/TheRiddler.java
@@ -2,24 +2,46 @@ package ifs_ints_and_loops;
 //    Copyright (c) The League of Amazing Programmers 2013-2017
 //    Level 0
 
-
+import javax.swing.JOptionPane;
 
 public class TheRiddler {
 
-	public static void main(String[] args) {
+public static void main(String[] args) {
 
-		// 1. Make a variable to hold the score
-
+		// 1. Make a variable to hold the score int
+            int score=0;
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
-
+           String TheRiddler=JOptionPane.showInputDialog("what has four legs but cant walk");
 		// 4. If they got the answer right, pop up "correct!" and increase the score by one
+               if(TheRiddler.equals("chair")) {
+            	   JOptionPane.showMessageDialog(null,"you got 2 points");
+                  score=score+2;
+               }
 
-		// 5. Otherwise, say "wrong" and tell them the answer
-
-		// 6. Add some more riddles
-
-		// 2. Make a pop up to show the score.
-		
-	}
+		// 5. Otherwise, say "wrong" and t	String birthday=JOptionPane.showInputDialog("enter birthday");ell them the answer
+                 else {
+                	 
+                 JOptionPane.showMessageDialog(null,"aww you tried!");
+                 }
+               
+               
+              
+               
+               
+               TheRiddler=JOptionPane.showInputDialog("what has four eyes but cant see");	
+		if(TheRiddler.equals("mississippi")) {
+			JOptionPane.showMessageDialog(null,"you got 2 points");
+		     score=score+2;
+		}else {
+			JOptionPane.showMessageDialog(null,"oh nooo!");
+		}
+       		 
+       		 
+       JOptionPane.showMessageDialog(null,"your score is "+score);		 
+}		 // 6. Add some more riddles
+       			
+                 		// 2. Make a pop up to show the score.
+		 
 }
+	
 
